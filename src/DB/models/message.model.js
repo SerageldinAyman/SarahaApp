0287c307e47b8e713b122c0ca3f9f23cc78913b1
Serageldin-Ayman <serag.eldin.ayman9@gmail.com>
2025-03-14 21:34:38 +0200

    init commit

--- src/DB/models/message.model.js
+++ src/DB/models/message.model.js
@@ -3,7 +3,7 @@ import { model, Schema, Types } from "mongoose";
 const messageSchema = new Schema(
   {
     body: { type: String, required: true },
-    sender: { type: Types.ObjectId, ref: "User" },
+    sender: { type: Types.ObjectId, ref: "User" /* اسم الmodel في ال ref*/ },
     receiver: { type: Types.ObjectId, ref: "User" },
   },
   { timestamps: true }
