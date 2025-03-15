ef6a1a6998fe465527ddf3c05e672b06f742834f
Serageldin-Ayman <serag.eldin.ayman9@gmail.com>
2025-03-15 20:41:09 +0200

    auth implementation

--- src/DB/models/message.model.js
+++ src/DB/models/message.model.js
@@ -3,8 +3,12 @@ import { model, Schema, Types } from "mongoose";
 const messageSchema = new Schema(
   {
     body: { type: String, required: true },
-    sender: { type: Types.ObjectId, ref: "User" /* اسم الmodel في ال ref*/ },
-    receiver: { type: Types.ObjectId, ref: "User" },
+    sender: {
+      type: Types.ObjectId,
+      ref: "User" /* اسم الmodel في ال ref*/,
+      required: true, //malhash 3elaka eno anonymous wla la2
+    },
+    receiver: { type: Types.ObjectId, ref: "User", required: true },
   },
   { timestamps: true }
 );
